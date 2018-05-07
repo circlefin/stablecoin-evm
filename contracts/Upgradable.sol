b54499b023ab252aa7e170a87ee5c2b02d693014
Oliver Hudson <ohudson@circle.com>
2018-05-07 15:35:49 -0400

    Eliminates isUpgraded extra variable

--- contracts/Upgradable.sol
+++ contracts/Upgradable.sol
@@ -7,7 +7,6 @@ pragma solidity ^0.4.18;
 contract Upgradable {
 
   address upgrader;
-  bool isContractUpgraded = false;
   address upgradedAddress;
   event Upgraded(address newContractAddress);
 
@@ -23,7 +22,7 @@ contract Upgradable {
    * @dev Checks if contract has been upgraded
   */
   function isUpgraded() public view returns (bool) {
-    return isContractUpgraded;
+    return upgradedAddress != 0x0;
   }
 
   /**
@@ -31,7 +30,6 @@ contract Upgradable {
    * @param _contractAddress address The address of the new contract
   */
   function upgrade(address _contractAddress) onlyUpgrader public {
-    isContractUpgraded = true;
     upgradedAddress = _contractAddress;
     Upgraded(upgradedAddress);
   }
