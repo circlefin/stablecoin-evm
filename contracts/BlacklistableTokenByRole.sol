e878b89ef3ac480f92530582eccccc2fa9ffa18b
Erik Tierney <eztierney@circle.com>
2018-06-12 13:51:34 -0400

    Move EternalStorage related contracts to storage directory

--- contracts/BlacklistableTokenByRole.sol
+++ contracts/BlacklistableTokenByRole.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.18;
 
 import './PausableTokenByRole.sol';
-import './EternalStorageUpdater.sol';
+import "./storage/EternalStorageUpdater.sol";
 
 /**
  * @title Blacklistable Token
