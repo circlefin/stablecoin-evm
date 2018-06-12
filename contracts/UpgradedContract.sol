3522ed2e34f47208475b00f9f4eb19a472d25f6b
Erik Tierney <eztierney@circle.com>
2018-06-12 14:43:42 -0400

    Whitespace cleanup - switch to 4 spaces everywhere  - matches solidity style guide  - should be no code changes

--- contracts/UpgradedContract.sol
+++ contracts/UpgradedContract.sol
@@ -7,9 +7,11 @@ import './../lib/openzeppelin/contracts/token/ERC20/ERC20.sol';
  * @dev ERC20 interface
 */
 contract UpgradedContract is ERC20 {
-	
-	function approveViaPriorContract(address _from, address _spender, uint256 _value) public returns (bool);
-	function transferViaPriorContract(address _from, address _to, uint256 _value) public returns (bool);
-	function transferFromViaPriorContract(address _sender, address _from, address _to, uint256 _value) public returns (bool);
+
+    function approveViaPriorContract(address _from, address _spender, uint256 _value) public returns (bool);
+
+    function transferViaPriorContract(address _from, address _to, uint256 _value) public returns (bool);
+
+    function transferFromViaPriorContract(address _sender, address _from, address _to, uint256 _value) public returns (bool);
 
 }
