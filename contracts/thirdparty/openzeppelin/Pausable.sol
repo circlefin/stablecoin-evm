9df23e9a3fdd8dc082c0dd3ac211dd46ae318059
Erik Tierney <eztierney@circle.com>
2018-06-14 13:52:42 -0400

    Added pauser role to Pausable  - added modifier onlyPauser  - switched payse/unpause to use onlyPauser  - added constructor to init pauser  - added updatePauser method to change the pauser, guarded by onlyOwner

--- contracts/thirdparty/openzeppelin/Pausable.sol
+++ contracts/thirdparty/openzeppelin/Pausable.sol
@@ -1,20 +1,29 @@
 pragma solidity ^0.4.23;
 
 
-import "../ownership/Ownable.sol";
+import "./Ownable.sol";
 
 
 /**
  * @title Pausable
  * @dev Base contract which allows children to implement an emergency stop mechanism.
  * Based on openzeppelin tag v1.10.0 commit: feb665136c0dae9912e08397c1a21c4af3651ef3
+ * Modifications:
+ * 1) Added pauser role, switched pause/unpause to be onlyPauser
  */
 contract Pausable is Ownable {
   event Pause();
   event Unpause();
+  event PauserChanged(address indexed newAddress);
 
+
+  address public pauser;
   bool public paused = false;
 
+
+  constructor(address _pauser) public {
+    pauser = _pauser;
+  }
 
   /**
    * @dev Modifier to make a function callable only when the contract is not paused.
@@ -33,9 +42,17 @@ contract Pausable is Ownable {
   }
 
   /**
+   * @dev throws if called by any account other than the pauser
+   */
+  modifier onlyPauser() {
+    require(msg.sender == pauser);
+    _;
+  }
+
+  /**
    * @dev called by the owner to pause, triggers stopped state
    */
-  function pause() onlyOwner whenNotPaused public {
+  function pause() onlyPauser whenNotPaused public {
     paused = true;
     emit Pause();
   }
@@ -43,8 +60,17 @@ contract Pausable is Ownable {
   /**
    * @dev called by the owner to unpause, returns to normal state
    */
-  function unpause() onlyOwner whenPaused public {
+  function unpause() onlyPauser whenPaused public {
     paused = false;
     emit Unpause();
   }
+
+  /**
+   * @dev update the pauser role
+   */
+  function updatePauser(address _newPauser) onlyOwner public {
+    pauser = _newPauser;
+    emit PauserChanged(pauser);
+  }
+
 }
