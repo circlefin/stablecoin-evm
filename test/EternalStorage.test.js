f93fc78b0aa104ee9a4adb10607f397c546964d0
mirathewhite <mira.belenkiy@gmail.com>
2018-06-19 09:38:58 -0400

    looking for hash function collisions

--- test/EternalStorage.test.js
+++ test/EternalStorage.test.js
@@ -91,4 +91,153 @@ contract('Eternal Storage Tests', function (accounts) {
         var actualOwner = await storage.owner.call();
         assert.equal(storageOwner, actualOwner, "owner should not change.");
     });
+
+    ///// SETTER Positive tests /////
+    it('setAllowed', async function () {
+        var expectedAmount = 867;
+        await storage.setAllowed(arbitraryAccount, minterAccount, expectedAmount, { from: storageOwner });
+        var actualAmount = await storage.getAllowed(arbitraryAccount, minterAccount);
+        (expectedAmount).should.be.bignumber.equal(actualAmount);
+    });
+
+    it('setBalance', async function () {
+        var expectedAmount = 7589035478901354;
+        await storage.setBalance(arbitraryAccount, expectedAmount, { from: storageOwner });
+
+        var actualAmount = await storage.getBalance(arbitraryAccount);
+        (expectedAmount).should.be.bignumber.equal(actualAmount);
+    });
+
+    it('setBalances', async function () {
+        var expectedAmount1 = 867534278;
+        var expectedAmount2 = 342;
+        await storage.setBalances(arbitraryAccount, expectedAmount1, minterAccount, expectedAmount2, { from: storageOwner });
+
+        var actualAmount1 = await storage.getBalance(arbitraryAccount);
+        (expectedAmount1).should.be.bignumber.equal(actualAmount1);
+        var actualAmount2 = await storage.getBalance(minterAccount);
+        (expectedAmount2).should.be.bignumber.equal(actualAmount2);
+    });
+
+    it('setTotalSupply', async function () {
+        var expectedAmount = 7587684752969;
+        await storage.setTotalSupply(expectedAmount, { from: storageOwner });
+
+        var actualAmount = await storage.getTotalSupply();
+        (expectedAmount).should.be.bignumber.equal(actualAmount);
+    });
+
+    // (check what happens if I sign a big number instread of true/false - will it overwrite anything else in the mapping)
+    it('setBlacklisted', async function () {
+        await storage.setBlacklisted(arbitraryAccount, true, { from: storageOwner });
+        assert.equal(true, await storage.isBlacklisted(arbitraryAccount));
+
+        await storage.setBlacklisted(arbitraryAccount, false);
+        assert.equal(false, await storage.isBlacklisted(arbitraryAccount));
+    });
+
+    it('setMinterAllowed', async function () {
+        var expectedAmount = 6542682969;
+        await storage.setMinterAllowed(minterAccount, expectedAmount, { from: storageOwner });
+
+        var actualAmount = await storage.getMinterAllowed(minterAccount);
+        (expectedAmount).should.be.bignumber.equal(actualAmount);
+
+        var isMinter = await storage.isMinter(minterAccount);
+        assert.equal(false, isMinter);
+    });
+
+    it('setMinter', async function () {
+        await storage.setMinter(minterAccount, true);
+        var isMinter = await storage.isMinter(minterAccount, { from: storageOwner });
+        assert.equal(true, isMinter);
+
+        await storage.setMinter(minterAccount, false);
+        var notMinter = await storage.isMinter(minterAccount, { from: storageOwner });
+        assert.equal(false, notMinter);
+
+    });
+
+    it('setBlacklisted bigInt', async function () {
+        await storage.setBlacklisted(arbitraryAccount, 4, { from: storageOwner });
+        assert.equal(true, await storage.isBlacklisted(arbitraryAccount));
+    });
+
+    it('setBlacklisted long address (right) no collision', async function () {
+        let shortAddress = 0xafce3ad7c92f4fd7e1b8fad9f;
+        let longAdress = 0xafce3ad7c92f4fd7e1b8fad9f000000000000000;
+
+        await storage.setBlacklisted(shortAddress, true, { from: storageOwner });
+        assert.equal(false, await storage.isBlacklisted(longAdress));
+    });
+
+    it('setBlacklisted long address (right) no collision', async function () {
+        let shortAddress = 0x1212;
+        let longAdress = 0x121200000000;
+
+        await storage.setBlacklisted(longAdress, true, { from: storageOwner });
+        assert.equal(false, await storage.isBlacklisted(shortAddress));
+    });
+
+    it('setBlacklisted long address (left) collision', async function () {
+        let longAddress = 0x000000000000000afce3ad7c92f4fd7e1b8fad9f;
+        let shortAddress = 0xafce3ad7c92f4fd7e1b8fad9f;
+
+        await storage.setBlacklisted(longAdress, true, { from: storageOwner });
+        assert.equal(false, await storage.isBlacklisted(shortAddress));
+    });
+    /*
+        it('setAllowed long address (left) collision', async function () {
+            //       let address1 = 0x000000000000000afce3ad7c92f4fd7e1b8fad9f;
+            let address1 = 0xbbbbbbbbbbbbbbbbbbbbbbbbb;
+            let address2 = 0xccccccccccccccc;
+            let address1a = 0xbbbbbbbbbbbbbbbbbbbbbbbbb;
+            let address2a = 0xbbbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccc;
+            let shortAddress = 0xafce3ad7c92f4fd7e1b8fad9f;
+    
+            await storage.setAllowed(address1, address2, 100, { from: storageOwner });
+    
+            let result = await storage.getAllowed(address1a, address2);
+            result.equal.
+            console.log("result: " + result.toString());
+        });
+    
+        it('setAllowed long address (zero) collision', async function () {
+            let address1 = 0xaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbb;
+            let address2 = 0xccccccccccccccc0000000000000000000000000;
+            let address1a = 0x0000000000000000000000000aaaaaaaaaaaaaaa;
+            let address2a = 0xbbbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccc;
+            let shortAddress = 0xafce3ad7c92f4fd7e1b8fad9f;
+    
+            await storage.setAllowed(address1, address2, 100, { from: storageOwner });
+    
+            let result = await storage.getAllowed(address1a, address2);
+            console.log("result: " + result.toString());
+        });*/
+
+
+    it('balances long address (zero) collision', async function () {
+        let shortAdress1 = 0xbbbbbbbbbbbbbbbbbbbbbbbbb;
+        let address1 = 0x000000000000000bbbbbbbbbbbbbbbbbbbbbbbbb;
+
+        let address2 = 0xccccccccccccccc0000000000000000000000000;
+        let shortAddress2 = 0xccccccccccccccc;
+
+        await storage.setBalance(shortAdress1, 100, { from: storageOwner });
+        await storage.setBalance(shortAddress2, 200, { from: storageOwner });
+
+        let balance1 = await storage.getBalance(address1);
+        let balance2 = await storage.getBalance(address2);
+
+        console.log("balance1: " + balance1.toString());
+        console.log("balance2: " + balance2.toString());
+    });
+
+    it('setBlacklisted test', async function () {
+        let longAddress = 0x000000000000000afce3ad7c92f4fd7e1b8fad9f;
+        let shortAddress = 0xafce3ad7c92f4fd7e1b8fad9f;
+
+        await storage.setBlacklisted(longAddress, true, { from: storageOwner });
+        assert.equal(false, await storage.isBlacklisted(shortAddress));
+    });
 });
