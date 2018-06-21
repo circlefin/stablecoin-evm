da4dedc04d03fe78e87735f2de921abdd1981c59
Alec Schaefer <aschaefer@circle.com>
2018-06-21 14:02:15 -0400

    removing coverageEnv files (that were added accidentally)

--- coverageEnv/Dockerfile.ganache
+++
@@ -1,6 +0,0 @@
-FROM node:9-alpine
-
-RUN npm i npm@latest -g \
- && npm install -g ganache-cli
-
-ENTRYPOINT ["ganache-cli"]
--- coverageEnv/migrations/2_deploy_usdc.js
+++
@@ -1,9 +0,0 @@
-var FiatToken = artifacts.require("./FiatToken.sol");
-
-module.exports = function(deployer, network, accounts) {
-  var a = accounts[0];
-  var s;
-  var ft;
-  return deployer.deploy(FiatToken, "0x0", "USDC", "USDC", "USD", 6,
-    a, a, a, a, a);
-};
