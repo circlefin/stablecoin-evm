d7ad665e679c2e9d4faf5319d7582a7ec24c246c
Erik Tierney <eztierney@circle.com>
2019-04-05 13:12:37 -0400

    Fix addresses

--- migrations/3_deploy_master_minter.js
+++ migrations/3_deploy_master_minter.js
@@ -1,10 +1,14 @@
 var MasterMinter = artifacts.require('./MasterMinter.sol');
+var FiatToken = artifacts.require('./FiatTokenProxy.sol')
 
 module.exports = function(deployer, network, accounts) {
 
-    var minterOwner = '0x496aacE5EEF1f4f505B67Af45de6Ba992785c124';
-    var fiatToken = '0x07865c6e87b9f70255377e024ace6630c1eaa37f';
-
+    if( network == "development" || network == "coverage") {
+        // Change these if deploying for real, these are deterministic
+        // address from ganache
+        var minterOwner = "0x3e5e9111ae8eb78fe1cc3bb8915d5d461f3ef9a9";
+        var fiatToken = FiatToken.address;
+    }
     console.log("deploying MasterMinter for fiat token at " + fiatToken);
     deployer.deploy(MasterMinter, fiatToken)
         .then( function(mm) {
