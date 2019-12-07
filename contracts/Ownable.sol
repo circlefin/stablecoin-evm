22aa763161e03db38628c02df0559ae61c1088a9
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2019-12-07 10:08:43 -1000

    Update Ownable.sol

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,5 +1,5 @@
 /**
-* Copyright CENTRE SECZ 2018
+* Copyright FutureCarbonCredits DAO/later to be LLC 2020
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy 
 * of this software and associated documentation files (the "Software"), to deal 
