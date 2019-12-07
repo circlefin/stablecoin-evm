fcf8019735c2f120e5aa7b0b3ec68a36ffc663ec
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2019-12-07 10:04:42 -1000

    Update FiatTokenV1.sol

--- contracts/FiatTokenV1.sol
+++ contracts/FiatTokenV1.sol
@@ -1,5 +1,5 @@
 /**
-* Copyright CENTRE SECZ 2018
+* Copyright FutureCarbonCredit DAO/later to be LLC 2018
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy 
 * of this software and associated documentation files (the "Software"), to deal 
