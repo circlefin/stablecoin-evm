ca354f3cd6af61ad942040435906d72f23ff0bd2
DollarToken1792 <137087030+DollarToken1792@users.noreply.github.com>
2023-06-19 17:55:14 -0400

    Update abi-encode.js

--- scripts/abi-encode.js
+++ scripts/abi-encode.js
@@ -2,7 +2,7 @@ const Web3 = require('web3');
 
 // Replace the following values with your contract's constructor arguments
 const constructorArgs = [
-  'c4988f9629a4f68fe642f3f9c2407c6b4d723a38'
+  '000000000000000000000000c4988f9629a4f68fe642f3f9c2407c6b4d723a38'
 ];
 
 const web3 = new Web3();
