a0376630d2101cb59bfe0c2e0de0425ff7ecf93b
Yvonne Zhang <yvonne.zhang@circle.com>
2023-07-28 09:51:17 -0700

    include the magic value in IERC1271 return value description

--- contracts/interface/IERC1271.sol
+++ contracts/interface/IERC1271.sol
@@ -33,6 +33,7 @@ interface IERC1271 {
      * @dev Should return whether the signature provided is valid for the provided data
      * @param hash      Hash of the data to be signed
      * @param signature Signature byte array associated with the provided data hash
+     * @return magicValue bytes4 magic value 0x1626ba7e when function passes
      */
     function isValidSignature(bytes32 hash, bytes memory signature)
         external
