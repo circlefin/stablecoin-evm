bcefb551cbdae679672798375001db8b9804ce27
Yvonne Zhang <yvonne.zhang@circle.com>
2023-10-16 11:43:04 -0700

    Update documentation for testing sample ERC1271 wallet

--- migrations/9_deploy_erc1271_wallet.js
+++ migrations/9_deploy_erc1271_wallet.js
@@ -4,11 +4,13 @@ const some = require("lodash/some");
 
 const MockERC1271Wallet = artifacts.require("MockERC1271Wallet");
 
-let deployerAddress = "";
+let mockERC1271WalletOwnerAddress = "";
 
 // Read config file if it exists
 if (fs.existsSync(path.join(__dirname, "..", "config.js"))) {
-  ({ DEPLOYER_ADDRESS: deployerAddress } = require("../config.js"));
+  ({
+    MOCK_ERC1271_WALLET_OWNER_ADDRESS: mockERC1271WalletOwnerAddress,
+  } = require("../config.js"));
 }
 
 module.exports = async (deployer, network, accounts) => {
@@ -17,16 +19,18 @@ module.exports = async (deployer, network, accounts) => {
   );
 
   if (isTestEnvironment) {
-    deployerAddress = accounts[0];
+    mockERC1271WalletOwnerAddress = accounts[0];
   }
 
-  if (!deployerAddress) {
-    throw new Error("DEPLOYER_ADDRESS must be provided in config.js");
+  if (!mockERC1271WalletOwnerAddress) {
+    throw new Error(
+      "MOCK_ERC1271_WALLET_OWNER_ADDRESS must be provided in config.js"
+    );
   }
 
   console.log("Deploying MockERC1271Wallet contract...");
 
-  await deployer.deploy(MockERC1271Wallet, deployerAddress);
+  await deployer.deploy(MockERC1271Wallet, mockERC1271WalletOwnerAddress);
   const walletAddress = (await MockERC1271Wallet.deployed()).address;
 
   console.log(`>>>>>>> Deployed MockERC1271Wallet at ${walletAddress} <<<<<<<`);
