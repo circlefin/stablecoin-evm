8517fb9768435d6117bcfa5394681d471b2dee04
Yvonne Zhang <yvonne.zhang@circle.com>
2024-01-04 08:55:43 -0800

    Refactor scripts to account for constructor and fallback

--- scripts/mergeAbi.js
+++ scripts/mergeAbi.js
@@ -27,19 +27,25 @@ async function mergeAbi(contractA, contractB) {
   const { abi: abiA } = require(`../build/contracts/${contractA}.json`);
   const { abi: abiB } = require(`../build/contracts/${contractB}.json`);
 
-  const mergedAbi = abiA.concat(abiB);
+  const eventsA = getAbiObjByType(abiA, "event");
+  const eventsB = getAbiObjByType(abiB, "event");
+  const functionsA = getAbiObjByType(abiA, "function");
+  const functionsB = getAbiObjByType(abiB, "function");
 
-  mergedAbi.sort((a, b) => {
-    if (a.type === b.type) {
-      // when a and b are both events or both functions, sort by name in alphabetical order
-      return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
-    } else if (a.type === "events") {
-      // event interfaces are placed before function interfaces
-      return -1;
-    } else {
-      return 1;
-    }
-  });
+  const alphabeticalSort = (a, b) => {
+    return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+  };
+
+  // Include constructor and fallback objects of contract A
+  const constructorAndFallbackA = abiA.filter(
+    (obj) => obj.type !== "event" && obj.type !== "function"
+  );
+
+  const events = eventsA.concat(eventsB);
+  const functions = functionsA.concat(functionsB);
+  events.sort(alphabeticalSort);
+  functions.sort(alphabeticalSort);
+  const mergedAbi = events.concat(functions).concat(constructorAndFallbackA);
 
   const outputName = `merged_${contractA}_${contractB}.json`;
   console.log(`Generated merged abi at ${outputName}`);
@@ -48,13 +54,17 @@ async function mergeAbi(contractA, contractB) {
   return mergedAbi;
 }
 
+function getAbiObjByType(abi, type) {
+  return abi.filter((obj) => obj.type === type);
+}
+
 async function main() {
   const argv = process.argv.slice(2);
   const [contractA, contractB] = argv;
 
   if (!contractA || !contractB) {
     throw new Error(
-      "Usage: yarn execScript scripts/mergeAbi.js <CONTRACT_A> <CONTRACT_B> "
+      "Usage: yarn execScript scripts/mergeAbi.js <CONTRACT_A> <CONTRACT_B> (Constructor and fallback of Contract A will be used)"
     );
   }
 
