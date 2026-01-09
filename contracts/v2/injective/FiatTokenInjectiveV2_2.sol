ea66d1536fd2ed88ea2cf40f75f3eceb1904bd8b
Zheng Wang <zhenghui.wang@circle.com>
2026-01-09 18:18:14 +0000

    Copyright

--- contracts/v2/injective/FiatTokenInjectiveV2_2.sol
+++ contracts/v2/injective/FiatTokenInjectiveV2_2.sol
@@ -1,5 +1,5 @@
 /**
- * Copyright 2025 Circle Internet Group, Inc. All rights reserved.
+ * Copyright 2026 Circle Internet Group, Inc. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
  *
